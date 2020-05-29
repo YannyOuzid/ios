c0b5126b86bd329fac6adb9748def4da4c7a989c
Cyril Lamirand @DEV <Contact.cyril1@gmail.com>
2020-05-29 17:24:23 +0200

    Push du seigneur

--- src/app/map-page/map-page.component.ts
+++ src/app/map-page/map-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild, ElementRef } from "@angular/core";
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { Router } from '@angular/router';
+
+declare var google:any;
 
 @Component({
   selector: 'app-map-page',
@@ -7,8 +12,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapPageComponent implements OnInit {
 
-  constructor() { }
+  map: any;
+
+  @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
+
+  constructor(public router : Router) { }
+
+  ionViewDidEnter() {
+    this.showMap();
+  }
+
+  showMap() {
+    const location = new google.maps.LatLng(48.8464, 2.3481);
+    const options = {
+      center: location,
+      zoom: 10,
+      disableDefaultUI: false
+    }
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+  }
 
   ngOnInit() {}
+
+  goToParcours(){
+    this.router.navigateByUrl('parcours')
+  }
+
+  goToHome(){
+    this.router.navigateByUrl('')
+  }
+
+  goToProfil(){
+    this.router.navigateByUrl('profil')
+  }
+
+  goToMap(){
+    this.router.navigateByUrl('map')
+  }
 
 }
