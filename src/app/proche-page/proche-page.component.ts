4a3614792fc0c9e489d062df2ff4e607c2bbd30b
Cyril Lamirand @DEV <Contact.cyril1@gmail.com>
2020-05-29 02:31:01 +0200

    Dieu vous tend sa main
    - Modification du header de l'appli (ajout du script CDN Google Map + Clé API)
    - Ajout de la map dans : Proche
    - Un peu de mise en page

--- src/app/proche-page/proche-page.component.ts
+++ src/app/proche-page/proche-page.component.ts
@@ -1,13 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild, ElementRef } from "@angular/core";
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+
+declare var google:any;
 
 @Component({
   selector: 'app-proche-page',
   templateUrl: './proche-page.component.html',
   styleUrls: ['./proche-page.component.scss'],
 })
+
 export class ProchePageComponent implements OnInit {
 
-  constructor() { }
+  map: any;
+
+  @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
+
+  constructor() {}
+
+  ionViewDidEnter() {
+    this.showMap();
+  }
+
+  showMap() {
+    const location = new google.maps.LatLng(48.8464, 2.3481);
+    const options = {
+      center: location,
+      zoom: 10,
+      disableDefaultUI: false
+    }
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+  }
 
   ngOnInit() {}
 
