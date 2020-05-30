4bfd8d92d7af200ce252bc951a887d674caea0a2
Cyril Lamirand @DEV <Contact.cyril1@gmail.com>
2020-05-30 17:38:19 +0200

    Push 17h38

--- src/app/tutorial-page/tutorial-page.component.ts
+++ src/app/tutorial-page/tutorial-page.component.ts
@@ -1,14 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-tutorial-page',
-  templateUrl: './tutorial-page.component.html',
-  styleUrls: ['./tutorial-page.component.scss'],
+  selector: 'slides-example',
+  template: `
+    <ion-content>
+      <ion-slides pager="false" [options]="slideOpts">
+        <ion-slide>
+          <h1>Slide 1</h1>
+          
+        </ion-slide>
+        <ion-slide>
+          <h1>Slide 2</h1>
+        </ion-slide>
+        <ion-slide>
+          <h1>Slide 3</h1>
+        </ion-slide>
+      </ion-slides>
+    </ion-content>
+  `
 })
-export class TutorialPageComponent implements OnInit {
+export class TutorialPageComponent {
 
-  constructor() { }
-
+  constructor() {}
   ngOnInit() {}
-
+}
+export class SlideExample {
+  // Optional parameters to pass to the swiper instance.
+  // See http://idangero.us/swiper/api/ for valid options.
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400
+  };
+  constructor() {}
 }
